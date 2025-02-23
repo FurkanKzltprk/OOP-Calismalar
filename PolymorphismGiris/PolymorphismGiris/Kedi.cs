e9fb235882a6695193cd638be74f234c5ff80f1e
FurkanKzltprk <kiziltoprakfurkan001@gmail.com>
2025-02-23 15:00:11 +0300

    Polymorphism ile ilgli alıştırma

---
+++ PolymorphismGiris/PolymorphismGiris/Kedi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismGiris
+{
+    public class Kedi : Hayvan
+    {
+        public Kedi(string isim) : base(isim)
+        {
+
+        }
+        //  override ile bir metodu ezmek istiyorsun 
+        //  o metot subclassta 'virtual' ile tanımlanmalı.
+        public override void konus()
+        {
+           Console.WriteLine(getIsim() + " miyavlıyor.... ");
+        }
+
+    }
+}
---
+++ PolymorphismGiris/PolymorphismGiris/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismGiris
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Hayvan hayvan = new Hayvan("Hayvan");
+             hayvan.konus();
+
+             Hayvan kedi = new Kedi("maya"); // buraya dikkat
+             kedi.konus();                   // Hayvan sınıfından kedi nesnesi 
+                                             // Kedi sınıfınını consturctoruna !!!
+
+             Kopek kopek = new Kopek("karabaş");
+             kopek.konus();
+
+             Console.ReadLine();*/
+
+
+            HayvanlariKonustur(new Kedi("minnoş"));  //farklı bir yaklaşım
+            HayvanlariKonustur(new Kopek("Karabaş"));
+
+            Console.ReadLine();
+        }
+        public static void HayvanlariKonustur(Hayvan hayvan) 
+        //bir tip tüm tipler gibi davranabildiği 
+        //hepsini tek bir metot parametresi olarak alabiliyor.   
+        {
+            hayvan.konus();
+        }
+    }
+}
