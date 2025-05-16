8fa0d65dda3ff0dfefa14c60637ac279340f35f9
FurkanKzltprk <kiziltoprakfurkan001@gmail.com>
2025-05-16 10:43:16 +0300

    9.hafta ve interface konusuyla ilgili çalışmalar eklendi

---
+++ NTP-Ders/NTP-Ders9-I/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTP_Ders9_I
+{
+    internal class Program
+    {
+
+        interface IHayvan
+        {
+            void sesCikar(); //gövdesiz metot
+
+        }
+
+
+        class Kedi : IHayvan
+        {
+            public void sesCikar()
+            {
+                Console.WriteLine("meow");
+            }
+        }
+
+        class Kopek : IHayvan
+        {
+            public void sesCikar() 
+            
+            {
+                Console.WriteLine("Anlar arkadan havlar lar lar lar hav hav hav");
+
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+
+
+            
+            Kedi kedi = new Kedi();
+            kedi.sesCikar();
+
+            IHayvan hayvan = new Kopek();
+            hayvan.sesCikar();
+
+
+
+        }
+    }
+}
